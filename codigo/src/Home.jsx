b25151f07f9fb1c0c275d09c305ef24532b882e7
Lucas CBar <lucas.cbar2001@gmail.com>
2023-09-26 17:07:04 -0300

    Subindo o codigo

---
+++ codigo/src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Titulo from './Titulo'
+
+const Home = () => {
+  return <section>
+    <Titulo texto="Home" />
+    <p> Está é a home do site. </p>
+  </section>
+}
+
+export default Home
---
+++ codigo/src/Produto.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Produto = ({ nome, propriedades }) => {
+  return (
+    <div style={{ border: '1px solid', margin: '1rem 0', padding: '1rem' }} >
+      <p> {nome}</p>
+      <ul>
+        <li>
+          {propriedades.map((propriedade) => (
+            <li> {propriedade} </li>
+          ))}
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Produto
